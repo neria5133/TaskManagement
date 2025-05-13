b4bbb5afc7fc0ee821ac96775d3cbe139fcda3c1
neryab <brhvz5133@gmail.com>
2025-05-13 13:41:45 +0300

    סיום אפליקציה לפני בדיקת דרישות

--- app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -8,26 +8,21 @@ import android.text.SpannableString;
 import android.text.style.ImageSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.time.LocalDate;
 
 public class MainActivity extends BaseActivity {
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button addButton = findViewById(R.id.buttonAddTask);
+        addButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddTask.class);
+            startActivity(intent);
         });
     }
 }
